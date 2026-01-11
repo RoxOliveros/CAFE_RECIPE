da1f5ed9f446692cbc6e48efb0644cde433c7255
jes <jesterresma@gmail.com>
2026-01-11 18:22:58 +0800

    commit

--- get-user-recipes.php
+++ get-user-recipes.php
@@ -1,9 +1,9 @@
 <?php
-session_start(); // ✅ REQUIRED
 header('Content-Type: application/json');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+session_start();
 require_once 'config/database.php';
 
 if (!isset($_SESSION['user_id'])) {
